701d21ace7642754279243dcab00997d4b2bf424
joaokage1 <j_vgomes@hotmail.com>
2020-05-07 13:20:18 -0300

    finish basics with this app

--- Apps/firstApp/src/app/feed/feed.page.ts
+++ Apps/firstApp/src/app/feed/feed.page.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feed.page.scss'],
 })
 export class FeedPage implements OnInit {
-
+  public nome_usuario:string;
+  
   constructor() { }
 
+  public mostraNome(): string{
+    this.nome_usuario = "Toddynho";
+
+    return this.nome_usuario;
+  }
+
   ngOnInit() {
+    this.mostraNome();
   }
 
 }
